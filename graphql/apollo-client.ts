52f4d93f0979da3428e2ab406bebca273c2aa24c
victorhugo-js <vt.hugo.2021@gmail.com>
2022-11-22 23:40:42 -0300

    api

--- graphql/apollo-client.ts
+++ graphql/apollo-client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "https://find-places-oo0adrl0g-hxsggsz.vercel.app/",
+  uri: "https://find-places.vercel.app/api/graphql",
   cache: new InMemoryCache(),
 });
